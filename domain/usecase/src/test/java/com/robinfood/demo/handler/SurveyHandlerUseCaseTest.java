a06050c6ad920cd3501e17b85896989b45eb820b
andreshincapier <luis.hincapie@sofka.com.co>
2021-09-15 21:49:05 -0500

    Update: Add SurveyHandlerUseCaseTest test

--- domain/usecase/src/test/java/com/robinfood/demo/handler/SurveyHandlerUseCaseTest.java
+++ domain/usecase/src/test/java/com/robinfood/demo/handler/SurveyHandlerUseCaseTest.java
@@ -1,6 +1,9 @@
 package com.robinfood.demo.handler;
 
+import com.robinfood.demo.dto.SurveyQuestionDTO;
+import com.robinfood.demo.entity.Question;
 import com.robinfood.demo.entity.Survey;
+import com.robinfood.demo.repository.QuestionRepository;
 import com.robinfood.demo.repository.SurveyRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,12 +11,13 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import java.util.List;
-
+import static java.util.List.of;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
+import static reactor.core.publisher.Flux.fromIterable;
 
 
 @RunWith(MockitoJUnitRunner.class)
@@ -25,10 +29,14 @@ public class SurveyHandlerUseCaseTest {
     @Mock
     private SurveyRepository surveyRepository;
 
+    @Mock
+    private QuestionRepository questionRepository;
+
     @Before
     public void init() {
-        Flux<Survey> list = Flux.fromIterable(List.of(survey));
-        when(surveyRepository.findAll()).thenReturn(list);
+        when(surveyRepository.findAll()).thenReturn(fromIterable(of(survey)));
+        when(surveyRepository.findById(any())).thenReturn(Mono.just(survey));
+        when(questionRepository.findAllBySurveyId(any())).thenReturn(fromIterable(of(question)));
     }
 
     @Test
@@ -38,12 +46,33 @@ public class SurveyHandlerUseCaseTest {
                 .verifyComplete();
     }
 
+    @Test
+    public void shouldFindAllQuestionsBySurveyId() {
+        StepVerifier.create(useCase.findAllQuestionsBySurveyId("something"))
+                .expectNext(surveyQuestionDTO)
+                .verifyComplete();
+    }
+
     private final Survey survey = Survey
             .builder()
             .id("one")
             .name("two")
-            .description("two")
-            .status("10044848L")
+            .description("description")
+            .status("ACTIVE")
             .build();
 
+    private final Question question = Question
+            .builder()
+            .id("123")
+            .content("two")
+            .surveyId("two")
+            .status("ACTIVE")
+            .build();
+
+    private final SurveyQuestionDTO surveyQuestionDTO = SurveyQuestionDTO
+            .builder()
+            .id("one")
+            .name("two")
+            .questionList(of(question))
+            .build();
 }
